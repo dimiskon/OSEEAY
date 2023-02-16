17312cccf2244b3770dea186fd909e1d7d9ce180
dimiskon <konstantinou.dimitris@outlook.com>
2023-02-16 13:46:59 +0200

    WIP on FE

--- client/osseay/src/components/Navbar.js
+++ client/osseay/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import Logo from '../assets/110pm_EMBL-transparent.png';
@@ -6,23 +6,31 @@ import '../styles/Navbar.css';
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 const Navbar = () => {
+  const [openLinks, setOpenLinks] = useState(false);
+
   return (
     <div className='navbar'>
       <div className='left_side'>
         <img alt='' src={Logo}></img>
       </div>
-      <div className='mid_side'> 
-         110ΠΜ - ΟΣΣΕΑΥ   
+      <div className='mid_side' id={openLinks ? 'open' : 'close'}> 
+        <text> 110ΠΜ - ΟΣΣΕΑΥ </text>   
+        <div className='hiddenLinks'>
+          <Link to='/'> ΑΡΧΙΚΗ</Link>
+          <Link to='/staffs'> ΠΡΟΣΩΠΙΚΟ </Link>
+          <Link to='/homes'> ΟΙΚΗΜΑΤΑ </Link>
+          <Link to='/requests'> ΑΙΤΗΣΕΙΣ </Link>
+        </div>
       </div>
       <div className='right_side'>
-        <Link to='/'> Home </Link>
+        <Link to='/'> ΑΡΧΙΚΗ</Link>
         <Link to='/staffs'> ΠΡΟΣΩΠΙΚΟ </Link>
         <Link to='/homes'> ΟΙΚΗΜΑΤΑ </Link>
         <Link to='/requests'> ΑΙΤΗΣΕΙΣ </Link>
         <button>
           <ReorderIcon />
         </button>
-      </div>
+     </div>
     </div>
   )
 }
